c90fdf284874d1b45a8052049eafc91d4923bc76
amanyebraheem <amanyebraheem9900@gmail.com>
2025-04-15 09:15:13 +0200

    Fixx

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,30 +2,22 @@
 
 import { ProductDetail } from "@/components/ui/product-detail";
 import { stripe } from "@/lib/stripe";
-import Stripe from "stripe";
+
 
 export default async function ProductsPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const product = await stripe.products.retrieve(params.id, {
+
+  const {id} = await params
+  const product = await stripe.products.retrieve(id, {
     expand: ["default_price"],
   });
 
-  // نسلسل البيانات لتكون قابلة للنقل إلى Client Component
-  const price = product.default_price as Stripe.Price;
+ 
 
-  const serializedProduct = {
-    id: product.id,
-    name: product.name,
-    description: product.description,
-    images: product.images,
-    default_price: {
-      unit_amount: price.unit_amount,
-      currency: price.currency,
-    },
-  };
+
 const plainProduct = JSON.parse(JSON.stringify(product));
   return <ProductDetail product={plainProduct} />;
 }
