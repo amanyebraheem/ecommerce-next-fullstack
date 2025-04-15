0736eec4d43a4f903561d06fdf283f2993d43912
amanyebraheem <amanyebraheem9900@gmail.com>
2025-04-15 10:11:28 +0200

    fixxs

--- app/checkout/checkout-action.tsx
+++ app/checkout/checkout-action.tsx
@@ -1,36 +1,72 @@
 
 
+// 'use server';
+
+// import { stripe } from "@/lib/stripe";
+// import { CartItem } from "@/store/cart-store";
+
+// export const checkoutAction = async (formData: FormData): Promise<{ url: string }> => {
+//   const itemsJson = formData.get("items");
+
+//   if (!itemsJson || typeof itemsJson !== "string") {
+//     throw new Error("Invalid cart data");
+//   }
+
+//   const items: CartItem[] = JSON.parse(itemsJson);
+
+//   const line_items = items.map((item) => ({
+//     price_data: {
+//       currency: "cad",
+//       product_data: { name: item.name },
+//       unit_amount: item.price,
+//     },
+//     quantity: item.quantity,
+//   }));
+
+//   const session = await stripe.checkout.sessions.create({
+//     payment_method_types: ["card"],
+//     line_items,
+//     mode: "payment",
+//     success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
+//     cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout`,
+//   });
+
+//   return { url: session.url! };
+// };
+
 'use server';
 
 import { stripe } from "@/lib/stripe";
 import { CartItem } from "@/store/cart-store";
+import { redirect } from "next/navigation";
 
-export const checkoutAction = async (formData: FormData): Promise<{ url: string }> => {
-  const itemsJson = formData.get("items");
+export const checkoutAction = async (formData: FormData): Promise<void> => {
 
-  if (!itemsJson || typeof itemsJson !== "string") {
-    throw new Error("Invalid cart data");
-  }
+const itemsJson = formData.get("items") as string 
+const items = JSON.parse(itemsJson)
+const line_items = items.map((item: CartItem) => ({
 
-  const items: CartItem[] = JSON.parse(itemsJson);
+price_data: {
 
-  const line_items = items.map((item) => ({
-    price_data: {
-      currency: "cad",
-      product_data: { name: item.name },
-      unit_amount: item.price,
-    },
-    quantity: item.quantity,
-  }));
+  currency: "cad",
+product_data: (name: item.name),
+unit_amount: item .price,
 
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items,
-    mode: "payment",
-    success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
-    cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout`,
-  });
+},
 
-  return { url: session.url! };
+quantity: item.quantity,
+
+}));
+
+const session = await stripe.checkout.sessions.create({
+
+payment_method_types: ["card"],
+line_items,
+mode: "payment" ,
+success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/success`,
+cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/checkout`,
+
+});
+
+redirect(session.url! );
 };
-
